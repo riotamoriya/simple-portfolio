eaccf601736ea4470544a1a29aa7811359637e1f
riotamoriya <riotamoriya@gmail.com>
2024-05-29 13:02:34 +0900

    マークダウン形式に対応

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,5 +131,14 @@ module.exports = {
       },
     },
 
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+      },
+    },
   ],
 };
